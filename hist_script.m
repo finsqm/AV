42dc4f9d18038b3eeea685382c53854437bb720c
Alexander Przybylski <s1205189@frasquita.inf.ed.ac.uk>
2016-02-04 20:58:13 +0000

    loop to identify circles in each image

--- hist_script.m
+++ hist_script.m
@@ -2,96 +2,122 @@
 thres = 0.25;
 
 
-a_hsv = hsv_img_cell{87};
-a_hsv = a_hsv - bg_img_cell{3}; 
+for img_idx = 1 : 210
+    
+    a_hsv = hsv_img_cell{img_idx};
+    a_hsv = a_hsv - bg_img_cell{3}; 
+    
+    %img to black/white
+    a_bw = a_hsv(:,:,2) >= thres;
+    
+    %map over all pixels to the left of the dancers
+    a_bw(:, 1:320) = 0;
+    %map pixels below the dancers
+    a_bw(240:480, :) = 0;
+    %map pixels to the right of the dancers
+    a_bw(:, 540:640) = 0;
+    
+    %dilate img
+    se = strel('disk', 6);
+    a_bw = imdilate(a_bw, se);
+    
+    
+    %full labelled image
+    [labelled, num] = bwlabel(a_bw,4);
 
-a_bw = a_hsv(:,:,2) >= thres;
+    STATS = regionprops(labelled, {'Area', 'BoundingBox', 'Centroid', 'Solidity', 'MajorAxisLength',... 
+        'MinorAxisLength'});
+    
+    %get rid of small objects
+    idx = find([STATS.Area] > 800);
+    bw2 = ismember(labelled, idx);
 
-%se = strel('diamond', 1);
-%a_bw = imerode(a_bw, se);
+    
+   
 
-%map over all pixels to the left of the dancers
-a_bw(:, 1:320) = 0;
-%map pixels below the dancers
-a_bw(240:480, :) = 0;
-%map pixels to the right of the dancers
-a_bw(:, 540:640) = 0;
+    [labelled, num] = bwlabel(bw2,4);
 
-se = strel('disk', 6);
-a_bw = imdilate(a_bw, se);
+    STATS = regionprops(labelled, {'Area', 'BoundingBox', 'Centroid', 'Solidity', 'MajorAxisLength',... 
+        'MinorAxisLength', 'Orientation'});
+    
+    %identify two people sticking together
+    idx = find([STATS.Area] > 1600);
+    bw_fats = ismember(labelled, idx);
+    
+    
+    %create separate image without people sticking together
+    idx = find([STATS.Area] <= 1600);
+    bw_no_fats = ismember(labelled, idx);
+    
+    
+    
+    %display the image with circles
+    %imshow(img_cell{img_idx});
+    imshow(bw2);
+    
+    [labelled, num] = bwlabel(bw_fats,4);
+    
+    if num > 0
+    
+        STATS = regionprops(labelled, {'Area', 'BoundingBox', 'Centroid', 'Solidity', 'MajorAxisLength',... 
+            'MinorAxisLength', 'Orientation'});
 
+        % based on axis of greatest width, we will chop big blobs 
+        % perpendicular to that axis
 
+        orient_angle = -1 * [STATS.Orientation];
 
-[labelled, num] = bwlabel(a_bw,4);
+        cents = cat(1, STATS.Centroid);
+        majAx = cat(1, STATS.MajorAxisLength)
+        minAx = cat(1, STATS.MinorAxisLength);
+        
+        quarters = repmat(0.25, num, 1);
+        
+        % TODO -
+        % Handle case where fat blob contain 3 people
+        % 2 people majAx = ~65
+        % 3 people majAx = ~117
+        % Based on majAx length, either split into 2 people or 3 people
+        
+        cent1_x = cents(:,1) + ( quarters(:) .* majAx(:) .* cosd(orient_angle(:)) ); 
+        cent1_y = cents(:,2) + ( quarters(:) .* majAx(:) .* sind(orient_angle(:)) );
 
-STATS = regionprops(labelled, {'Area', 'BoundingBox', 'Centroid', 'Solidity', 'MajorAxisLength',... 
-    'MinorAxisLength'});
+        cent2_x = cents(:,1) - ( quarters(:) .* majAx(:) .* cosd(orient_angle(:)) );
+        cent2_y = cents(:,2) - ( quarters(:) .* majAx(:) .* sind(orient_angle(:)) );
 
+        
 
-%get rid of small objects
-idx = find([STATS.Area] > 800);
-bw2 = ismember(labelled, idx);
+        %get the fat circles info
+        f_centers = cat(1, [cent1_x cent1_y], [cent2_x cent2_y]);        
+        majorAx = cat(1, majAx./2);
+        minorAx = cat(1, minAx./2);
+        both_axes = cat(1, [majorAx minorAx], [majorAx minorAx]);
+        diameters = mean(both_axes, 2);
+        f_radii = diameters/2;
 
+        hold on
+        viscircles(f_centers, f_radii);
+        hold off
+    end
+    
+    %get the non-fat circles info
+    [labelled2, num2] = bwlabel(bw_no_fats,4);
 
+    STATS2 = regionprops(labelled2, {'Area', 'BoundingBox', 'Centroid', 'Solidity', 'MajorAxisLength',... 
+        'MinorAxisLength', 'Orientation'});
 
-% based on axis of greatest width, chop big blobs perpendicular to
-% that axis
-
-[labelled, num] = bwlabel(bw2,4);
-
-STATS = regionprops(labelled, {'Area', 'BoundingBox', 'Centroid', 'Solidity', 'MajorAxisLength',... 
-    'MinorAxisLength', 'Orientation'});
-
-idx = find([STATS.Area] > 1500);
-bw_fats = ismember(labelled, idx);
-
-%create new image with no fat blobs
-idx = find([STATS.Area] <= 1500);
-bw_no_fats = ismember(labelled, idx);
-
-
-%
-
-[labelled, num] = bwlabel(bw_fats,4);
-
-STATS = regionprops(labelled, {'Area', 'BoundingBox', 'Centroid', 'Solidity', 'MajorAxisLength',... 
-    'MinorAxisLength', 'Orientation'});
-
-orient_angle = STATS.Orientation;
-
-cent1_x = STATS.Centroid(1) + ( 0.25 * STATS.MajorAxisLength * cosd(orient_angle) ); 
-cent1_y = STATS.Centroid(2) + (0.25 * STATS.MajorAxisLength * sind(orient_angle) );
-
-cent2_x = STATS.Centroid(1) - ( 0.25 * STATS.MajorAxisLength * cosd(orient_angle) );
-cent2_y = STATS.Centroid(2) - (0.25 * STATS.MajorAxisLength * sind(orient_angle) );
-
-%------------
-
-%draw the circles
-
-%centers = cat(1, STATS.Centroid);
-%majorAx = cat(1, STATS.MajorAxisLength);
-%minorAx = cat(1, STATS.MinorAxisLength);
-%diameters = mean([majorAx minorAx], 2);
-%[radii] = diameters/2;
-
-%temp for testing --- 
-% the centroid seems ON POINT, 
-% but the areas of the circles are crap
-centers = cat(1, [cent1_x cent1_y], [cent2_x cent2_y])
-majorAx = cat(1, STATS.MajorAxisLength/2);
-minorAx = cat(1, STATS.MinorAxisLength/2);
-both_axes = cat(1, [majorAx minorAx], [majorAx minorAx]);
-diameters = mean(both_axes, 2);
-[radii] = diameters/2;
-
-imshow(bw_fats);
-
-hold on
-viscircles(centers, radii);
-hold off
-
-
-
+    centers = cat(1, STATS2.Centroid);
+    majorAx = cat(1, STATS2.MajorAxisLength);
+    minorAx = cat(1, STATS2.MinorAxisLength);
+    diameters = mean([majorAx minorAx], 2);
+    [radii] = diameters/2;
 
 
+    %display the circles on the image
+    hold on
+    viscircles(centers, radii);
+    hold off
+
+    w = waitforbuttonpress;
+    
+end
